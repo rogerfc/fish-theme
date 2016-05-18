4c9f5a2143629d2ec4485b9cef4df7d7eef27c99
Roger Firpo Curcoll <roger.firpo@gmail.com>
2016-05-18 18:53:07 +0200

    initial commit

---
+++ fish_greeting.fish
@@ -0,0 +1,12 @@
+# Set global color styles, for example:
+#
+# function ruficu_error
+#   set_color -o red
+# end
+#
+# function ruficu_normal
+#   set_color normal
+#
+
+function fish_greeting
+end
